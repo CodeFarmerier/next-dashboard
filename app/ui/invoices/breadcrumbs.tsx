11cd0d75e036c11f71466e980a56a7ced0f17ac2
CodeFarmerier <mengandxa@163.com>
2024-04-25 20:07:24 +0800

    fix：初始化next learn模板

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -26,7 +26,7 @@ export default function Breadcrumbs({
           >
             <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
             {index < breadcrumbs.length - 1 ? (
-              <span className="mx-3 inline-block">/</span>
+              <span className=" mx-3 inline-block">/</span>
             ) : null}
           </li>
         ))}
